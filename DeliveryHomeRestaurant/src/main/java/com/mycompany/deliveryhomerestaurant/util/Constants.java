c0a1964d2fd3213f1b344d0fbb0f93f7cf1b6f13
Francesco Di Giacomo <francesco.digiacomo1@student.univaq.it>
2025-06-29 01:59:18 +0200

    Re-Fixed API_KEY issue

--- DeliveryHomeRestaurant/src/main/java/com/mycompany/deliveryhomerestaurant/util/Constants.java
+++ DeliveryHomeRestaurant/src/main/java/com/mycompany/deliveryhomerestaurant/util/Constants.java
@@ -1,6 +1,20 @@
 package com.mycompany.deliveryhomerestaurant.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Constants {
-    public static final String INDIRIZZO_RISTORANTE = "Piazza Duomo 1, L'Aquila";
-    public static final String API_KEY = "AIzaSyCAlrUNZS-AwZE83ZwuG9U3LswiC6zng00";
+    public static final String API_KEY;
+    public static final String INDIRIZZO_RISTORANTE = "Piazza Duomo 1, L'Aquila";;
+
+    static {
+        Properties prop = new Properties();
+        try (InputStream input = Constants.class.getClassLoader().getResourceAsStream("config.properties")) {
+            prop.load(input);
+            API_KEY = prop.getProperty("api.key");
+        } catch (IOException e) {
+            throw new RuntimeException("API_KEY non trovata");
+        }
+    }
 }
